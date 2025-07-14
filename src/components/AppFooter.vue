034e349d2d2ee0557360671dd6c6c96cc4adcbab
zvictorliu <zvictorliui@outlook.com>
2025-07-14 20:36:25 +0800

    style(Header&Footer): 设置Header和Footer的样式
    - 以黑白为主
    - 样式文件拆开，方便复用

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,17 +6,5 @@
 
 <script setup>
 // 这里可以添加props或其他逻辑
+import '../style/footer.css'
 </script>
-
-<style scoped>
-.app-footer {
-  background: #333;
-  color: #fff;
-  padding: 1rem;
-  text-align: center;
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-}
-</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,14 +6,5 @@
 
 <script setup>
 // 这里可以添加props或其他逻辑
+import '../style/header.css'
 </script>
-
-<style scoped>
-.app-header {
-  background: #42b983;
-  color: white;
-  padding: 1rem;
-  text-align: center;
-  width: 100%;
-}
-</style>
